7fa85594d3b4ed502fa76e1cb6950ed342c0d2c9
Miguel López Ulloa <mikauran@hotmail.com>
2019-07-28 12:45:08 -0500

    Setup Kenzanians Spring boot
    Setup Eclipse artifacts

---
+++ src/main/java/com/mikauran/kenzanians/KenzaniansApplication.java
@@ -0,0 +1,13 @@
+package com.mikauran.kenzanians;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenzaniansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KenzaniansApplication.class, args);
+	}
+
+}
