03c71c10691ed6a571ba2f88d00a41ac4a8c037a
Miguel López Ulloa <mikauran@hotmail.com>
2019-07-29 13:06:30 -0500

    Considering SpringSecurity config
    Configuration java class to spring security

---
+++ src/main/java/com/mikauran/kenzanians/config/SpringSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.mikauran.kenzanians.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // Create 2 Employees Rolls for Kensanians App
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("employee").password("{noop}password").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
+
+    }
+
+    // Secure the end points with HTTP Basic authentication
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                //HTTP Basic authentication
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/employee/**").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/employee/").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/employee/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PATCH, "/employees/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/employee/**").hasRole("ADMIN")
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
+    }
+
+   
+}
+
